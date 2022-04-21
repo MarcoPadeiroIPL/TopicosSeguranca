0f42878531989e3dcfc269cae97e474982ecdaca
TomasMartinsMoura <2211866@my.ipleiria.pt>
2022-04-21 17:27:14 +0100

    pequenas alterações UI

--- ProjetoTopicosSegurança/ProjetoTopicosSegurança/Form1.Designer.cs
+++ ProjetoTopicosSegurança/ProjetoTopicosSegurança/Form1.Designer.cs
@@ -29,115 +29,118 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBoxChavePublica = new System.Windows.Forms.TextBox();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
             this.buttonEnviarChave = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBoxChat = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxChat = new System.Windows.Forms.TextBox();
+            this.textBoxMensagem = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(32, 22);
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(102, 60);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(105, 20);
+            this.label1.Size = new System.Drawing.Size(49, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Chave pública:";
+            this.label1.Text = "Login:";
             // 
-            // textBoxChavePublica
+            // textBoxUsername
             // 
-            this.textBoxChavePublica.Location = new System.Drawing.Point(143, 22);
-            this.textBoxChavePublica.Name = "textBoxChavePublica";
-            this.textBoxChavePublica.Size = new System.Drawing.Size(205, 27);
-            this.textBoxChavePublica.TabIndex = 1;
+            this.textBoxUsername.Location = new System.Drawing.Point(58, 94);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(144, 27);
+            this.textBoxUsername.TabIndex = 1;
             // 
             // buttonEnviarChave
             // 
-            this.buttonEnviarChave.Location = new System.Drawing.Point(374, 22);
+            this.buttonEnviarChave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(225)))), ((int)(((byte)(255)))), ((int)(((byte)(0)))));
+            this.buttonEnviarChave.Location = new System.Drawing.Point(71, 332);
             this.buttonEnviarChave.Name = "buttonEnviarChave";
             this.buttonEnviarChave.Size = new System.Drawing.Size(94, 29);
             this.buttonEnviarChave.TabIndex = 2;
             this.buttonEnviarChave.Text = "Enviar";
-            this.buttonEnviarChave.UseVisualStyleBackColor = true;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(32, 67);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(24, 20);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "IP:";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(62, 64);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(75, 27);
-            this.textBox1.TabIndex = 4;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(153, 67);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 20);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Porta:";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(205, 64);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(84, 27);
-            this.textBox2.TabIndex = 6;
-            // 
-            // textBoxChat
-            // 
-            this.textBoxChat.Location = new System.Drawing.Point(71, 128);
-            this.textBoxChat.Multiline = true;
-            this.textBoxChat.Name = "textBoxChat";
-            this.textBoxChat.Size = new System.Drawing.Size(338, 252);
-            this.textBoxChat.TabIndex = 7;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(32, 128);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(42, 20);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "Chat:";
+            this.buttonEnviarChave.UseVisualStyleBackColor = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(71, 386);
+            this.button1.Location = new System.Drawing.Point(694, 393);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(94, 29);
             this.button1.TabIndex = 9;
             this.button1.Text = "Enviar";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // buttonClose
+            // 
+            this.buttonClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(50)))), ((int)(((byte)(10)))));
+            this.buttonClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonClose.FlatAppearance.BorderSize = 0;
+            this.buttonClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonClose.ForeColor = System.Drawing.Color.Transparent;
+            this.buttonClose.Location = new System.Drawing.Point(753, -2);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(47, 27);
+            this.buttonClose.TabIndex = 10;
+            this.buttonClose.Text = "X";
+            this.buttonClose.UseVisualStyleBackColor = false;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(544, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Chat";
+            // 
+            // textBoxChat
+            // 
+            this.textBoxChat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(36)))), ((int)(((byte)(84)))));
+            this.textBoxChat.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxChat.ForeColor = System.Drawing.Color.White;
+            this.textBoxChat.Location = new System.Drawing.Point(334, 57);
+            this.textBoxChat.Multiline = true;
+            this.textBoxChat.Name = "textBoxChat";
+            this.textBoxChat.Size = new System.Drawing.Size(454, 329);
+            this.textBoxChat.TabIndex = 7;
+            // 
+            // textBoxMensagem
+            // 
+            this.textBoxMensagem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(36)))), ((int)(((byte)(84)))));
+            this.textBoxMensagem.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxMensagem.Font = new System.Drawing.Font("Cascadia Mono", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxMensagem.ForeColor = System.Drawing.Color.White;
+            this.textBoxMensagem.Location = new System.Drawing.Point(334, 393);
+            this.textBoxMensagem.Multiline = true;
+            this.textBoxMensagem.Name = "textBoxMensagem";
+            this.textBoxMensagem.Size = new System.Drawing.Size(354, 40);
+            this.textBoxMensagem.TabIndex = 11;
+            this.textBoxMensagem.Text = "Escreva uma mensagem!";
+            this.textBoxMensagem.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBoxMensagem.Enter += new System.EventHandler(this.textBoxMensagem_Enter);
+            this.textBoxMensagem.Leave += new System.EventHandler(this.textBoxMensagem_Leave);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(36)))), ((int)(((byte)(60)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxMensagem);
             this.Controls.Add(this.textBoxChat);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.buttonEnviarChave);
-            this.Controls.Add(this.textBoxChavePublica);
+            this.Controls.Add(this.textBoxUsername);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -148,14 +151,12 @@
         #endregion
 
         private Label label1;
-        private TextBox textBoxChavePublica;
+        private TextBox textBoxUsername;
         private Button buttonEnviarChave;
-        private Label label2;
-        private TextBox textBox1;
-        private Label label3;
-        private TextBox textBox2;
-        private TextBox textBoxChat;
-        private Label label4;
         private Button button1;
+        private Button buttonClose;
+        private Label label4;
+        private TextBox textBoxChat;
+        private TextBox textBoxMensagem;
     }
 }
--- ProjetoTopicosSegurança/ProjetoTopicosSegurança/Form1.cs
+++ ProjetoTopicosSegurança/ProjetoTopicosSegurança/Form1.cs
@@ -6,5 +6,33 @@ namespace ProjetoTopicosSegurança
         {
             InitializeComponent();
         }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxMensagem_Enter(object sender, EventArgs e)
+        {
+            if(textBoxMensagem.Text == "Escreva uma mensagem!")
+            {
+                textBoxMensagem.Text = "";
+               
+            }
+        }
+
+        private void textBoxMensagem_Leave(object sender, EventArgs e)
+        {
+            if (textBoxMensagem.Text == "")
+            {
+                textBoxMensagem.Text = "Escreva uma mensagem!";
+                
+            }
+        }
     }
 }

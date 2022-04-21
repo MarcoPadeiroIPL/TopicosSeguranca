6bda05b2f6c6b0484f5d7230658c02079935ce67
Marco Padeiro <2211868@my.ipleiria.pt>
2022-04-21 22:20:50 +0100

    Base chat

---
+++ Servidor/ClientHandler.cs
@@ -0,0 +1,66 @@
+﻿using EI.SI;
+using System;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Servidor
+{
+    internal class ClientHandler
+    {
+        // informacao do cliente
+        private TcpClient client; 
+        private int clientID;
+        internal ClientHandler(TcpClient client, int clientID)
+        {
+            this.client = client;
+            this.clientID = clientID;
+        }
+
+        internal void Handle()
+        {
+            // Criação de Thread
+            Thread thread = new Thread(threadHandler);
+            thread.Start();
+        }
+
+        private void threadHandler()
+        {
+            // Armazena a conexão do cliente
+            NetworkStream networkStream = this.client.GetStream();
+            ProtocolSI protocolSI = new ProtocolSI();
+
+            // Enquanto a transmissão com o cliente não acabar
+            while(protocolSI.GetCmdType() != ProtocolSICmdType.EOT)
+            {
+                // lê a informação da possivel mensagem enviada pelo cliente
+                int bytesRead = networkStream.Read(protocolSI.Buffer, 0, protocolSI.Buffer.Length);
+
+                // cria um ACK - um sinal que o recetor (servidor) envia para confirmar a receção da mensagem
+                byte[] ack;
+
+                switch(protocolSI.GetCmdType())
+                {
+                    // caso a informação recebida seja uma mensagem
+                    case ProtocolSICmdType.DATA:
+                        string msg = "Cliente " + clientID + ": " + protocolSI.GetStringFromData();
+                        Console.WriteLine(msg);
+                        // confirma que recebeu a mensagem pelo envio de um ack
+                        ack = protocolSI.Make(ProtocolSICmdType.ACK);
+                        networkStream.Write(ack, 0, ack.Length);
+                        break;
+                    // caso a informação recebida tenha sido de fim de transmissão
+                    case ProtocolSICmdType.EOT:
+                        Console.WriteLine("Cliente {0} desligado.", clientID);
+                        // confirma que recebeu a mensagem pelo envio de um ack
+                        ack = protocolSI.Make(ProtocolSICmdType.ACK);
+                        networkStream.Write(ack, 0, ack.Length);
+                        break;
+                }
+
+            }
+            // encerramento de ligação com o cliente
+            networkStream.Close();
+            client.Close();
+        }
+    }
+}
---
+++ Servidor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using EI.SI;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Servidor // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        // porto do servidor
+        private const int PORT = 5000;
+
+        static void Main(string[] args)
+        {
+            // criação de um servidor na porta 5000
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Loopback, PORT);
+            // procura por clientes
+            TcpListener listener = new TcpListener(endpoint);
+            listener.Start();
+            ProtocolSI protocolSI = new ProtocolSI();
+            int clientConter = 0;
+            Console.WriteLine("A iniciar o servidor " + IPAddress.Loopback + "...");
+            Console.WriteLine("A definir a porta do servidor...");
+            Console.WriteLine("PORTA: " + PORT);
+            Console.WriteLine("Servidor pronto!\n");
+            Console.WriteLine("A aguardar clientes...\n");
+
+            
+            while(true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                clientConter++;
+                Console.WriteLine("Cliente {0} ligado", clientConter);
+
+                ClientHandler handler = new ClientHandler(client, clientConter);
+                handler.Handle();
+
+            }
+
+        } 
+    }
+}

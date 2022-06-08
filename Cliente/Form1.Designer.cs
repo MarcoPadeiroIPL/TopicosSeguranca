230172060587e12c88324156fb59b24f4ccc19dc
MarcoPadeiroIPL <2211868@my.ipleiria.pt>
2022-06-08 15:06:42 +0100

    small fix

---
+++ Cliente/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace ProjetoTopicosSegurança
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.textBoxChat = new System.Windows.Forms.TextBox();
+            this.textBoxMensagem = new System.Windows.Forms.TextBox();
+            this.buttonMinimize = new System.Windows.Forms.Button();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonEnviar = new System.Windows.Forms.Button();
+            this.buttonRegister = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(36)))), ((int)(((byte)(60)))));
+            this.label1.Font = new System.Drawing.Font("Cascadia Mono", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(50, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username:";
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(36)))), ((int)(((byte)(84)))));
+            this.textBoxUsername.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxUsername.Font = new System.Drawing.Font("Cascadia Mono", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxUsername.ForeColor = System.Drawing.Color.White;
+            this.textBoxUsername.Location = new System.Drawing.Point(50, 118);
+            this.textBoxUsername.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(126, 14);
+            this.textBoxUsername.TabIndex = 1;
+            this.textBoxUsername.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxUsername_KeyPress);
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(36)))), ((int)(((byte)(84)))));
+            this.buttonLogin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonLogin.Font = new System.Drawing.Font("Cascadia Mono", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonLogin.ForeColor = System.Drawing.Color.White;
+            this.buttonLogin.Location = new System.Drawing.Point(67, 198);
+            this.buttonLogin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(82, 22);
+            this.buttonLogin.TabIndex = 2;
+            this.buttonLogin.TabStop = false;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = false;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(50)))), ((int)(((byte)(10)))));
+            this.buttonClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonClose.FlatAppearance.BorderSize = 0;
+            this.buttonClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonClose.ForeColor = System.Drawing.Color.Transparent;
+            this.buttonClose.Location = new System.Drawing.Point(656, -4);
+            this.buttonClose.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(41, 20);
+            this.buttonClose.TabIndex = 10;
+            this.buttonClose.TabStop = false;
+            this.buttonClose.Text = "X";
+            this.buttonClose.UseVisualStyleBackColor = false;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // textBoxChat
+            // 
+            this.textBoxChat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(36)))), ((int)(((byte)(84)))));
+            this.textBoxChat.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxChat.Font = new System.Drawing.Font("Cascadia Mono", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxChat.ForeColor = System.Drawing.Color.White;
+            this.textBoxChat.Location = new System.Drawing.Point(224, 38);
+            this.textBoxChat.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxChat.Multiline = true;
+            this.textBoxChat.Name = "textBoxChat";
+            this.textBoxChat.ReadOnly = true;
+            this.textBoxChat.Size = new System.Drawing.Size(453, 247);
+            this.textBoxChat.TabIndex = 7;
+            this.textBoxChat.TabStop = false;
+            // 
+            // textBoxMensagem
+            // 
+            this.textBoxMensagem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(36)))), ((int)(((byte)(84)))));
+            this.textBoxMensagem.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxMensagem.Font = new System.Drawing.Font("Cascadia Mono", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxMensagem.ForeColor = System.Drawing.Color.White;
+            this.textBoxMensagem.Location = new System.Drawing.Point(224, 303);
+            this.textBoxMensagem.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxMensagem.Name = "textBoxMensagem";
+            this.textBoxMensagem.PlaceholderText = "Escreva uma mensagem!";
+            this.textBoxMensagem.Size = new System.Drawing.Size(376, 14);
+            this.textBoxMensagem.TabIndex = 11;
+            this.textBoxMensagem.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxMensagem_KeyPress);
+            // 
+            // buttonMinimize
+            // 
+            this.buttonMinimize.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(36)))), ((int)(((byte)(60)))));
+            this.buttonMinimize.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonMinimize.FlatAppearance.BorderSize = 0;
+            this.buttonMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMinimize.ForeColor = System.Drawing.Color.Transparent;
+            this.buttonMinimize.Location = new System.Drawing.Point(609, -4);
+            this.buttonMinimize.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonMinimize.Name = "buttonMinimize";
+            this.buttonMinimize.Size = new System.Drawing.Size(41, 20);
+            this.buttonMinimize.TabIndex = 13;
+            this.buttonMinimize.TabStop = false;
+            this.buttonMinimize.Text = "-";
+            this.buttonMinimize.UseVisualStyleBackColor = false;
+            this.buttonMinimize.Click += new System.EventHandler(this.buttonMinimize_Click);
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(36)))), ((int)(((byte)(84)))));
+            this.textBoxPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxPassword.Font = new System.Drawing.Font("Cascadia Mono", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxPassword.ForeColor = System.Drawing.Color.White;
+            this.textBoxPassword.Location = new System.Drawing.Point(50, 162);
+            this.textBoxPassword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(126, 14);
+            this.textBoxPassword.TabIndex = 2;
+            this.textBoxPassword.UseSystemPasswordChar = true;
+            this.textBoxPassword.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxUsername_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(36)))), ((int)(((byte)(60)))));
+            this.label2.Font = new System.Drawing.Font("Cascadia Mono", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(50, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 16);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Password:";
+            // 
+            // buttonEnviar
+            // 
+            this.buttonEnviar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(36)))), ((int)(((byte)(84)))));
+            this.buttonEnviar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonEnviar.Font = new System.Drawing.Font("Cascadia Mono", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonEnviar.ForeColor = System.Drawing.Color.White;
+            this.buttonEnviar.Location = new System.Drawing.Point(606, 300);
+            this.buttonEnviar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonEnviar.Name = "buttonEnviar";
+            this.buttonEnviar.Size = new System.Drawing.Size(72, 22);
+            this.buttonEnviar.TabIndex = 16;
+            this.buttonEnviar.Text = "Enviar";
+            this.buttonEnviar.UseVisualStyleBackColor = false;
+            this.buttonEnviar.Click += new System.EventHandler(this.buttonEnviar_Click);
+            // 
+            // buttonRegister
+            // 
+            this.buttonRegister.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(36)))), ((int)(((byte)(84)))));
+            this.buttonRegister.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonRegister.Font = new System.Drawing.Font("Cascadia Mono", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonRegister.ForeColor = System.Drawing.Color.White;
+            this.buttonRegister.Location = new System.Drawing.Point(67, 239);
+            this.buttonRegister.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonRegister.Name = "buttonRegister";
+            this.buttonRegister.Size = new System.Drawing.Size(82, 22);
+            this.buttonRegister.TabIndex = 17;
+            this.buttonRegister.TabStop = false;
+            this.buttonRegister.Text = "Registar";
+            this.buttonRegister.UseVisualStyleBackColor = false;
+            this.buttonRegister.Click += new System.EventHandler(this.buttonRegister_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(36)))), ((int)(((byte)(60)))));
+            this.ClientSize = new System.Drawing.Size(693, 338);
+            this.Controls.Add(this.buttonRegister);
+            this.Controls.Add(this.buttonEnviar);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonMinimize);
+            this.Controls.Add(this.textBoxMensagem);
+            this.Controls.Add(this.textBoxChat);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBoxUsername;
+        private Button buttonLogin;
+        private Button buttonClose;
+        private TextBox textBoxChat;
+        private TextBox textBoxMensagem;
+        private Button buttonMinimize;
+        private TextBox textBoxPassword;
+        private Label label2;
+        private Button buttonEnviar;
+        private Button buttonRegister;
+    }
+}
---
+++ Servidor/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using EI.SI;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Security.Cryptography;
+
+namespace Servidor // Note: actual namespace depends on the project name.
+{
+    static class Globals
+    {
+        public static List<User> users = new List<User>(); // variavel global que armazena todos os users
+        //public static string currentPath = @"C:\temp\servidor\logs\log" + DateTime.Now.ToString("yy-MM-dd_HH:mm:ss") + ".txt";
+        public static string currentPath = Directory.GetCurrentDirectory() + "\\logs\\log" + DateTime.Now.ToString("yy/MM/dd__HH.mm.ss") + ".txt";
+    }
+    internal class Program
+    {
+        // porto do servidor
+        private const int PORT = 5000;
+
+        // criação da chave simetrica
+        private static Aes aes;
+
+        // criação da chave publica e privada
+
+        static void Main(string[] args)
+        {
+            // criação de um servidor na porta 5000
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Loopback, PORT);
+            // procura por clientes
+            TcpListener listener = new TcpListener(endpoint);
+            listener.Start();
+            ProtocolSI protocolSI = new ProtocolSI();
+            int clientCounter = 0;
+
+            // definição da chave simetrica (Criação da key e do IV)
+            aes = Aes.Create();
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            // definição da chave privada
+            string privKey = rsa.ToXmlString(true);
+            string pubKey = rsa.ToXmlString(false);
+
+            WriteToLog("A iniciar o servidor " + IPAddress.Loopback + "...");
+            WriteToLog("A definir a porta do servidor");
+            WriteToLog("PORTA: " + PORT);
+            WriteToLog("Servidor pronto!\n");
+            WriteToLog("A aguardar clientes...\n");
+
+            // vai à procura de clientes    
+            while(true)
+            {
+                // quando acha um cliente
+                TcpClient client = listener.AcceptTcpClient();
+
+                clientCounter++;
+                User currUtilizador = new User(client, false); // criação de nova instancia de um user 
+                Globals.users.Add(currUtilizador); // adiciona à lista global de todos os users
+
+
+                ClientHandler handler = new ClientHandler(currUtilizador, aes.Key, aes.IV, privKey, pubKey);
+                handler.Handle();
+            }
+        } 
+        public static void SendToEveryone(byte[] package) // faz um broadcast de uma mensagem para todos os clientes
+        {
+            foreach(User user in Globals.users)
+            {
+                if(user.GetLogin())
+                {
+                    NetworkStream networkStream = user.GetStream();
+                    networkStream.Write(package, 0, package.Length);
+                    networkStream.Flush();
+                }
+               
+            }
+        }
+        public static void WriteToLog(string msg)
+        {
+            File.WriteAllText("log" + DateTime.Now.ToString("yy_MM_dd__HH.mm.ss") + ".txt", msg);
+            Console.WriteLine(msg);
+            
+        }
+    }
+}
